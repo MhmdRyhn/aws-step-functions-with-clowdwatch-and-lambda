ff2daf0fceeeb5c29c6c451071b526294426b98c
MhmdRyhn <rayhan.cse12@gmail.com>
2020-06-25 11:57:33 +0600

    Convert assume role policy into data.aws_iam_policy_document

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -1,64 +1,61 @@
 # ---------- State machine role ---------------
+data "aws_iam_policy_document" "state_machine_assume_role_document" {
+  statement {
+    sid = "AutoGreetingStateMachineRole"
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["states.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
 resource "aws_iam_role" "state_machine_role" {
   name = "StateMachineRole"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "states.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": "AutoGreetingStateMachineRole"
-    }
-  ]
-}
-EOF
+  assume_role_policy = data.aws_iam_policy_document.state_machine_assume_role_document.json
 }
 
 
 # --------------- Basic lambda execution role ------------
+data "aws_iam_policy_document" "lambda_assume_role_document" {
+  statement {
+    sid = "BasicLambdaExecutionRole"
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
 resource "aws_iam_role" "basic_lambda_execution_role" {
   name = "BasicLambdaExecutionRole"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": "BasicLambdaExecutionRole"
-    }
-  ]
-}
-EOF
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_document.json
 }
 
 
 # ------------- CloudWatch role to trigger step functions ---------------
+data "aws_iam_policy_document" "cloudwatch_triggering_role_document" {
+  statement {
+    sid = "CloudwatchTriggeringStepFunctionsRole"
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["cloudwatch.amazonaws.com"]
+      type = "Service"
+    }
+    principals {
+      identifiers = ["events.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
 resource "aws_iam_role" "cloudwatch_triggering_step_functions_role" {
   name = "CloudwatchTriggeringStepFunctionsRole"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "cloudwatch.amazonaws.com",
-        "Service": "events.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": "CloudwatchTriggeringStepFunctionsRole"
-    }
-  ]
-}
-EOF
+  assume_role_policy = data.aws_iam_policy_document.cloudwatch_triggering_role_document.json
 }
 
 
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
+//terraform {
+//  required_version = ">=0.12.23"
+//  required_providers {
+//    archive = ">=1.3.0"
+//    aws = ">=2.67.0"
+//    template = ">=2.1.2"
+//  }
+//}
+
 provider "aws" {
   region = var.region
   access_key = var.access_key
